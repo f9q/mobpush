511d5e19d7da217899bdbde37ad1d1a554868ff4
k3e1 <wangliang@jiankangyangfan.com>
2019-12-15 14:38:47 +0800

    添加停止推送，重启推送

--- app/src/main/java/com/example/mobpush/MainActivity.kt
+++ app/src/main/java/com/example/mobpush/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.mobpush
 
+import android.app.ActivityManager
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.mob.pushsdk.MobPush
 import com.mob.pushsdk.MobPushCustomMessage
 import com.mob.pushsdk.MobPushNotifyMessage
 import com.mob.pushsdk.MobPushReceiver
+import java.io.File
 
 
 class MainActivity : AppCompatActivity(),MobPushReceiver {
@@ -18,14 +24,18 @@ class MainActivity : AppCompatActivity(),MobPushReceiver {
         setContentView(R.layout.activity_main)
 
         initMobPush()
+
+        var id = android.os.Process.myPid()
+        Log.e(TAG,"my process id = $id")
+
     }
 
     fun initMobPush(){
-        MobPush.setAlias("Alias")                   //设置别名
-        MobPush.addTags(arrayOf("taikang","tag2"))  //设置标签
+        MobPush.setAlias("Alias2t43232")            //设置别名
+        MobPush.addTags(arrayOf("tag3"))            //设置标签
         MobPush.setShowBadge(true)                  //默认是关闭的，设置true为打开显示角标，反之则为关闭显示角标
-
         MobPush.addPushReceiver(this)
+        MobPush.restartPush()
     }
 
     override fun onAliasCallback(p0: Context?, p1: String?, p2: Int, p3: Int) {
@@ -47,10 +57,49 @@ class MainActivity : AppCompatActivity(),MobPushReceiver {
     override fun onTagsCallback(p0: Context?, p1: Array<out String>?, p2: Int, p3: Int) {
         Log.e(TAG,"onTagsCallback")
         Log.e(TAG,"p1.sz = ${p1?.size}, p2 = $p2, p3 = $p3")
+        Log.e(TAG,"process id = " + android.os.Process.myPid())
     }
 
     override fun onNotifyMessageOpenedReceive(p0: Context?, p1: MobPushNotifyMessage?) {
         Log.e(TAG,"onNotifyMessageOpenedReceive")
         Log.e(TAG,"p1 = $p1")
+    }
+
+    fun onStopClicked(v : View){
+        while (true){
+            MobPush.cleanTags()
+            MobPush.clearAllNotification()
+            MobPush.clearLocalNotifications()
+            MobPush.removePushReceiver(this)
+            MobPush.stopPush()
+            var intent = Intent()
+            var pkg = "com.example.mobpush:mobservice"
+            intent.setPackage(pkg)
+            stopService(intent)
+            var am = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+            am.killBackgroundProcesses(pkg)
+            am.killBackgroundProcesses(packageName)
+            var processes = am.getRunningAppProcesses()
+            for (process in processes){
+                Log.e(TAG,"process id = " + process.pid + " name = " + process.processName)
+                var pkgList = process.pkgList
+                for (p in pkgList){
+                    Log.e(TAG,"pkg = " + p.toString())
+                }
+            }
+            android.os.Process.killProcess(processes[1].pid)
+            am.restartPackage(packageName)
+            SystemClock.sleep(200)
+        }
+    }
+    fun onRestartClicked(v : View){
+        MobPush.restartPush()
+        var ret = MobPush.isPushStopped()
+        Log.e(TAG,"stop = $ret")
+//        MobPush.setSilenceTime(11, 0,12, 0)
+    }
+    fun onIsStoppedClicked(v : View){
+        var ret = MobPush.isPushStopped()
+        Log.e(TAG,"stop = $ret")
     }
 }
